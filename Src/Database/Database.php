7065a9bed1f61a5575a0c4a21a6be2c583ae9db6
rayanebenoudia <rayane.benoudia@laplateforme.io>
2026-05-20 11:28:27 +0200

    je push

--- Src/Database/Database.php
+++ Src/Database/Database.php
@@ -2,29 +2,28 @@
 namespace Boutique\Database;
 use PDO;
 
-
-
-
 class Database {
     private static $instance = null;
     private PDO $pdo;
 
     private function __construct() {
-        $env = parse_ini_file(__DIR__ . '/../.env');
-        try {
-            $this->pdo = new PDO(
-                "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8",
-                $env['DB_USER'],
-                $env['DB_PASSWORD'],
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-                ]
-            );
-        } catch (\PDOException $e) {
-            die("Erreur de connexion à la base de données");
-        }
+    // Chemin absolu vers le .env
+    $env = parse_ini_file(__DIR__ . '/../../Config/.env');
+    
+    try {
+        $this->pdo = new PDO(
+            "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8",
+            $env['DB_USER'],
+            $env['DB_PASSWORD'],
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
+    } catch (\PDOException $e) {
+        die("Erreur de connexion : " . $e->getMessage());
     }
+}
 
     public static function getInstance(): self {
         if (self::$instance === null) {
